196c18107270c03dc59c31c35e6ae105ab19e2cb
LukastheHippo <lukas.the.hippo@gmail.com>
2024-12-27 08:16:35 -0600

    add fps cap

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from constants import *
 
 def main():
     pygame.init()
-    black = pygame.Color(0, 0, 0)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    fps = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, black)
+        pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        fps.tick(60)
 
     
 if __name__ == "__main__":

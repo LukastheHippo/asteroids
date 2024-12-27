661e339427671a2c057b7a12c97b4ea5dd7dd734
LukastheHippo <lukas.the.hippo@gmail.com>
2024-12-27 10:06:40 -0600

    added rotation and movement to player

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     fps = pygame.time.Clock()
     dt = 0
-
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         pygame.Surface.fill(screen, "black")
         player.draw(screen)
         pygame.display.flip()
-        fps.tick(60)
+        dt = fps.tick(60) / 1000
 
     
 if __name__ == "__main__":
